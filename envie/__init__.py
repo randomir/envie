2546b39f8df07e185c8ea04369b1fbe404da64d5
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-02-25 19:54:59 +0100

    python activation more robust, raises exception on any sign of trouble

--- envie/__init__.py
+++ envie/__init__.py
@@ -1,21 +1,33 @@
 from __future__ import print_function
-import os
+import os.path
 import subprocess
+
+
+class EnvieError(Exception):
+    """Envie-specific errors."""
+
 
 def activate(start_dir=None):
     """Looks for and then activates the closest virtual environment, but only 
     if a single closest env is found."""
-    
+
     try:
         output = subprocess.check_output("envie find -f -q", shell=True).strip('\n')
-        if not output:
-            return 1
+
         envs = output.split('\n')
-        if len(envs) != 1:
-            return 2
+        if len(envs) < 1:
+            raise EnvieError("No virtual environments find in the vicinity.")
+        elif len(envs) > 1:
+            raise EnvieError("Found more than one virtual environment.")
         envpath = envs[0]
+
     except subprocess.CalledProcessError as exc:
-        return 1
+        raise EnvieError("Failed to look for the closest virtual "\
+                         "environment via envie command.")
 
     activate_this = os.path.join(envpath, 'bin/activate_this.py')
+    if not os.path.isfile(activate_this):
+        raise EnvieError(
+            "Invalid virtual environment found (%s is missing)" % activate_this)
+
     execfile(activate_this, dict(__file__=activate_this))
