a105fb1a2917bb33fe4b6fd4eef5220240d8420f
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-03-29 22:47:44 +0200

    bumped to v0.4.21

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='envie',
-    version='0.4.20',
+    version='0.4.21',
     description="Bash helpers for navigating and managing Python VirtualEnvs.",
     long_description=open('README.rst').read(),
     author='Radomir Stevanovic',
