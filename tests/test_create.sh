e925ab4e10eafb743adf41519a12ba80a930aa70
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-04-10 03:26:23 +0200

    test: envie create

---
+++ tests/test_create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+test_envie_create_help() {
+    local bin=$(which envie)
+    env -i "$bin" create -h | grep 'Create Python (2/3) virtual environment'
+}
+
+test_envie_create() {
+    local dir=$(mktemp -u)
+    env -i "$(which envie)" create "$dir"
+    test -e "$dir/bin/python"
+    local code=$?
+    rm -rf "$dir"
+    return $code
+}
+
+test_envie_create_3() {
+    command -v python3 >/dev/null 2>&1 || return 0
+    local dir=$(mktemp -u)
+    env -i "$(which envie)" create -3 "$dir"
+    test -e "$dir/bin/python" && "$dir/bin/python" -V | grep "^Python 3";
+    local code=$?
+    rm -rf "$dir"
+    return $code
+}
+
+
+. unittest.inc && main
