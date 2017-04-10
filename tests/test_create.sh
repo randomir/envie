4abd886b55f0586541d60bea39c1719d5b648f6b
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-04-10 03:34:28 +0200

    test: added explicit py2 env create

--- tests/test_create.sh
+++ tests/test_create.sh
@@ -14,11 +14,21 @@ test_envie_create() {
     return $code
 }
 
+test_envie_create_2() {
+    command -v python2 >/dev/null 2>&1 || return 0
+    local dir=$(mktemp -u)
+    env -i "$(which envie)" create -2 "$dir"
+    test -e "$dir/bin/python" && "$dir/bin/python" -V 2>&1 | grep "^Python 2"
+    local code=$?
+    rm -rf "$dir"
+    return $code
+}
+
 test_envie_create_3() {
     command -v python3 >/dev/null 2>&1 || return 0
     local dir=$(mktemp -u)
     env -i "$(which envie)" create -3 "$dir"
-    test -e "$dir/bin/python" && "$dir/bin/python" -V | grep "^Python 3";
+    test -e "$dir/bin/python" && "$dir/bin/python" -V 2>&1 | grep "^Python 3";
     local code=$?
     rm -rf "$dir"
     return $code
