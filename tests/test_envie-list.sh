4f5f0763c67f6ba40e2cdb63f37361c12df51d7d
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-04-18 18:30:44 +0200

    test envie list: faked envierc

--- tests/test_envie-list.sh
+++ tests/test_envie-list.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+function _dump_test_config() {
+    # NB: $HOME == $polygon_dir
+    cat <<-END
+		_ENVIE_CONFIG_DIR="$HOME/.config/envie"
+		_ENVIE_USE_DB="1"
+		_ENVIE_DB_PATH="$HOME/.config/envie/locate.db"
+		_ENVIE_INDEX_ROOT="$HOME"
+		_ENVIE_CRON_INDEX="0"
+		_ENVIE_LS_INDEX="1"
+		_ENVIE_FIND_LIMIT_SEC="0.4"
+		_ENVIE_LOCATE_LIMIT_SEC="4"
+	END
+}
+
 setup() {
     tests_dir=$(dirname "$0")
     envie_bin=$(readlink -f "$tests_dir/../scripts/envie")
     polygon_dir=$(mktemp -d)
     cd "$polygon_dir"
-    echo "(envie sourced from $envie_bin)"
+    echo "(using envie from $envie_bin)"
     echo "(created polygon dir: $polygon_dir)"
+
+    # create few envs
     echo -n "(creating test environments in polygon dir..."
     local create_output
     create_output=$(
@@ -39,6 +55,23 @@ setup() {
         return 1
     else
         echo "DONE)"
+    fi
+
+    # fake envierc for testing
+    echo -n "(creating test envierc..."
+    HOME="$polygon_dir"
+    config_dir="$polygon_dir/.config/envie"
+    mkdir -p "$config_dir"
+    _dump_test_config >"$config_dir/envierc"
+    if (
+        set -e
+        . "$envie_bin"
+        [ "$_ENVIE_CONFIG_PATH" == "$config_dir/envierc" ] && (( _ENVIE_USE_DB ))
+    ); then
+        echo "OK)"
+    else
+        echo "FAILED)"
+        return 1
     fi
 }
 
