a1cef5c2a622cae32fd61eb404e740b3c5685efb
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-04-19 15:36:00 +0200

    test envie list: locate vs find case

--- tests/test_envie-list.sh
+++ tests/test_envie-list.sh
@@ -235,4 +235,79 @@ test_envie_list_locate_multiple_levels() (
 )
 
 
+# test list using find vs. locate rate
+
+test_envie_list_race_empty_from_cwd() (
+    cd "$polygon_dir/project_a/src"
+    local list=$("$envie_bin" list)
+    [ -z "$list" ]
+)
+
+test_envie_list_race_empty_from_path() (
+    local list=$("$envie_bin" list "$polygon_dir/project_a/src")
+    [ -z "$list" ]
+)
+
+test_envie_list_race_single_py3_from_cwd() (
+    cd "$polygon_dir/project_a"
+    local list=$("$envie_bin" list)
+    [ "$list" == "./env_a" ]
+)
+
+test_envie_list_race_single_py2_from_path() (
+    local list=$("$envie_bin" list "$polygon_dir/project_b")
+    [ "$list" == "$polygon_dir/project_b/env_b" ]
+)
+
+test_envie_list_race_multiple() (
+    cd "$polygon_dir/project_c"
+    local list=$("$envie_bin" list | sort)
+    local expected
+    expected=$(cat <<-END
+		./sub_a/env_ca1
+		./sub_a/env_ca2
+		./sub_a/env_ca3
+		./sub_b/env_cb
+		./sub_c/env_cc
+	END
+    )
+    echo "$list"
+    echo "$expected"
+    [ "$list" == "$expected" ]
+)
+
+test_envie_list_race_multiple_avoid_some() (
+    cd "$polygon_dir/project_c"
+    local list=$("$envie_bin" list . ./sub_a | sort)
+    local expected
+    expected=$(cat <<-END
+		./sub_b/env_cb
+		./sub_c/env_cc
+	END
+    )
+    echo "$list"
+    echo "$expected"
+    [ "$list" == "$expected" ]
+)
+
+test_envie_list_race_multiple_levels() (
+    cd "$polygon_dir"
+    local list=$("$envie_bin" list | sort)
+    local expected
+    expected=$(cat <<-END
+		./project_a/env_a
+		./project_b/env_b
+		./project_c/sub_a/env_ca1
+		./project_c/sub_a/env_ca2
+		./project_c/sub_a/env_ca3
+		./project_c/sub_b/env_cb
+		./project_c/sub_c/env_cc
+	END
+    )
+    echo "$list"
+    echo "$expected"
+    [ "$list" == "$expected" ]
+)
+
+
 . $(dirname "$0")/unittest.inc && main
