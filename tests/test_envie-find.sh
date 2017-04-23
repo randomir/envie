bf0cfb289a6f359c646b05031f4a78b013e72c43
Radomir Stevanovic <radomir.stevanovic@gmail.com>
2017-04-23 10:35:36 +0200

    envie find test: --locate branch

--- tests/test_envie-find.sh
+++ tests/test_envie-find.sh
@@ -87,4 +87,138 @@ test_envie_find_single_level_up_from_relpath() (
     [ "$list" == "project_a/env_a" ]
 )
 
+
+test_envie_find_normalize_abspath() (
+    local list=$("$envie_bin" find --find "$polygon_dir/project_a/../project_b")
+    [ "$list" == "$polygon_dir/project_b/env_b" ]
+)
+
+test_envie_find_normalize_relpath() (
+    cd "$polygon_dir/project_a"
+    local list=$("$envie_bin" find --find "../project_b/env_b/local/bin/")
+    [ "$list" == "../project_b/env_b" ]
+)
+
+
+test_envie_find_multiple_envs_multiple_level_up_from_relpath() (
+    local list=$("$envie_bin" find --find ".config/envie" | sort)
+    local expected
+    expected=$(cat <<-END
+		./project_a/env_a
+		./project_b/env_b
+		./project_c/sub_a/env_ca1
+		./project_c/sub_a/env_ca2
+		./project_c/sub_a/env_ca3
+		./project_c/sub_b/env_cb
+		./project_c/sub_c/env_cc
+	END
+    )
+    echo "$list"
+    echo "$expected"
+    [ "$list" == "$expected" ]
+)
+
+
+# test 'envie find' using locate
+
+test_envie_find_locate_inside_env_root_from_cwd() (
+    cd "$polygon_dir/project_a/env_a"
+    local list=$("$envie_bin" find --locate)
+    echo $list
+    [ "$list" == "." ]
+)
+
+test_envie_find_locate_inside_env_root_from_abspath() (
+    local list=$("$envie_bin" find --locate "$polygon_dir/project_a/env_a")
+    [ "$list" == "$polygon_dir/project_a/env_a" ]
+)
+
+test_envie_find_locate_inside_env_root_from_relpath() (
+    local list=$("$envie_bin" find --locate "project_a/env_a")
+    [ "$list" == "project_a/env_a" ]
+)
+
+
+test_envie_find_locate_inside_env_bin_from_cwd() (
+    cd "$polygon_dir/project_a/env_a/bin"
+    local list=$("$envie_bin" find --locate)
+    [ "$list" == ".." ]
+)
+
+test_envie_find_locate_inside_env_bin_from_abspath() (
+    local list=$("$envie_bin" find --locate "$polygon_dir/project_a/env_a/bin")
+    [ "$list" == "$polygon_dir/project_a/env_a" ]
+)
+
+test_envie_find_locate_inside_env_bin_from_relpath() (
+    local list=$("$envie_bin" find --locate "./project_a/env_a/bin")
+    [ "$list" == "project_a/env_a" ]
+)
+
+
+test_envie_find_locate_single_level_down_from_cwd() (
+    cd "$polygon_dir/project_a"
+    local list=$("$envie_bin" find --locate)
+    [ "$list" == "./env_a" ]
+)
+
+test_envie_find_locate_single_level_down_from_abspath() (
+    local list=$("$envie_bin" find --locate "$polygon_dir/project_a")
+    [ "$list" == "$polygon_dir/project_a/env_a" ]
+)
+
+test_envie_find_locate_single_level_down_from_relpath() (
+    local list=$("$envie_bin" find --locate "project_a")
+    [ "$list" == "project_a/env_a" ]
+)
+
+
+test_envie_find_locate_single_level_up_from_cwd() (
+    cd "$polygon_dir/project_a/src"
+    local list=$("$envie_bin" find --locate)
+    [ "$list" == "../env_a" ]
+)
+
+test_envie_find_locate_single_level_up_from_abspath() (
+    local list=$("$envie_bin" find --locate "$polygon_dir/project_a/src")
+    [ "$list" == "$polygon_dir/project_a/env_a" ]
+)
+
+test_envie_find_locate_single_level_up_from_relpath() (
+    local list=$("$envie_bin" find --locate "project_a/src")
+    [ "$list" == "project_a/env_a" ]
+)
+
+
+test_envie_find_locate_normalize_abspath() (
+    local list=$("$envie_bin" find --locate "$polygon_dir/project_a/../project_b")
+    [ "$list" == "$polygon_dir/project_b/env_b" ]
+)
+
+test_envie_find_locate_normalize_relpath() (
+    cd "$polygon_dir/project_a"
+    local list=$("$envie_bin" find --locate "../project_b/env_b/local/bin/")
+    [ "$list" == "../project_b/env_b" ]
+)
+
+
+test_envie_find_locate_multiple_envs_multiple_level_up_from_relpath() (
+    local list=$("$envie_bin" find --locate ".config/envie" | sort)
+    local expected
+    expected=$(cat <<-END
+		./project_a/env_a
+		./project_b/env_b
+		./project_c/sub_a/env_ca1
+		./project_c/sub_a/env_ca2
+		./project_c/sub_a/env_ca3
+		./project_c/sub_b/env_cb
+		./project_c/sub_c/env_cc
+	END
+    )
+    echo "$list"
+    echo "$expected"
+    [ "$list" == "$expected" ]
+)
+
+
 . $(dirname "$0")/unittest.inc && main
